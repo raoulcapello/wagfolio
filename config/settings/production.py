cd627147027e1badb4cc83ca830a01bc6b420de1
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2022-04-07 21:15:22 +0200

    Change WhiteNoise configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -75,7 +75,8 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "devfolio.utils.storages.MediaRootS3Boto3Storage"
