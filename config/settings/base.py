8c08ba0b574d44ab84fc6c4e24c0e64abfea4651
Raoul Capello <raoul@raoulcapello.nl>
2023-04-08 18:24:53 +0200

    Add CORS configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -313,6 +313,9 @@ REST_FRAMEWORK = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
+CORS_ALLOWED_ORIGINS = [
+    "https://www.raoulcapello.nl",
+]
 
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
