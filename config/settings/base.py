12b90acc553c8510df74cdc4282530db9383fa9c
Raoul Capello <raoul@raoulcapello.nl>
2023-04-08 18:25:58 +0200

    Add CORS configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -314,7 +314,7 @@ REST_FRAMEWORK = {
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOWED_ORIGINS = [
-    "https://www.raoulcapello.nl",
+    "http://localhost:8000",
 ]
 
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
